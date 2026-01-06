c855e8ad681b7cde960fe1862ab15329c88559a0
MustafinMP <mustafinmp@yandex.ru>
2026-01-06 23:30:33 +0500

    Add asynchronous token update

--- finam.py
+++ finam.py
@@ -1,6 +1,8 @@
+import asyncio
 import json
+import threading
 
-import requests
+import aiohttp
 
 from _services.account import AccountService
 from _services.assets import AssetService
@@ -16,18 +18,45 @@ class Finam:
         super().__init__()
         self._account_id = account_id
         self._user_token = user_token
-        response = requests.post(
-            f'{self._base_url}sessions',
-            data=json.dumps({'secret': self._user_token}),
-            headers={'Content-Type': 'application/json', 'Accept': 'application/json'}
-        )
-        self._jwt_token = response.json()['token']
+        self._jwt_token = None
+
+        self._lock = threading.Lock()
 
         self.account = AccountService(self._jwt_token, self._account_id, self._base_url)
         self.instruments = AssetService(self._jwt_token, self._account_id, self._base_url)
         self.orders = OrderService(self._jwt_token, self._account_id, self._base_url)
         self.market = MarketService(self._jwt_token, self._account_id, self._base_url)
         self.metrics = MetricsService(self._jwt_token, self._account_id, self._base_url)
+
+        self._jwt_thread = threading.Thread(
+            target=asyncio.run,
+            args=(self._update_jwt_token(), ),
+            daemon=True
+        )
+        self._jwt_thread.start()
+
+    async def _update_jwt_token(self) -> None:
+        session = aiohttp.ClientSession()
+        period_in_seconds = 5 #14 * 60 + 30
+
+        try:
+            while True:
+                with self._lock:
+                    async with session.post(
+                            f'{self._base_url}sessions',
+                            data=json.dumps({'secret': self._user_token}),
+                            headers={'Content-Type': 'application/json', 'Accept': 'application/json'}
+                    ) as response:
+                        self._jwt_token = (await response.json())['token']
+                        self.account._update_jwt(self._jwt_token)
+                        self.instruments._update_jwt(self._jwt_token)
+                        self.orders._update_jwt(self._jwt_token)
+                        self.market._update_jwt(self._jwt_token)
+                        self.metrics._update_jwt(self._jwt_token)
+
+                await asyncio.sleep(period_in_seconds)
+        finally:
+            await session.close()
 
     def set_account(self, account_id: str) -> None:
         self._account_id = account_id
@@ -39,3 +68,6 @@ class Finam:
 
     def _headers(self):
         return {"Authorization": f"{self._jwt_token}", 'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+    def __del__(self):
+        self._jwt_thread.join()
