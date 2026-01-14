763c4cb31f7adfda40b35d558b93d6b3ca6dba7d
Mustafin Maksim <74867408+MustafinMP@users.noreply.github.com>
2026-01-14 23:40:10 +0500

    Delete tests directory

--- tests/metrics.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-from config import FINAM_TOKEN, ACCOUNT_ID
-from finam_rest.finam import Finam
-
-finam = Finam(FINAM_TOKEN, ACCOUNT_ID)
-print('get last quote')
-print(finam.metrics.get_last_quote())
