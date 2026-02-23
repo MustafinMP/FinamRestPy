b3218be6ca51476636ab9eb5df2300b8fef753fe
musta <mustafinmp@yandex.ru>
2026-02-23 15:01:19 +0500

    Добавлены документирующие строки в модуль AssetService

--- finam_rest_py/services/assets.py
+++ finam_rest_py/services/assets.py
@@ -8,6 +8,14 @@ from finam_rest_py.services.base_service import AsyncBaseService
 
 class AssetService(AsyncBaseService):
     async def get_assets(self) -> list[Asset]:
+        """Получает список доступных инструментов, их описание.
+
+        Returns:
+            list[Asset]: список доступных инструментов.
+
+        Raises:
+            FinamResponseFailureException: если произошла ошибка запроса к серверу.
+        """
         response = await self._session.get('assets')
         if response.status_code == 200:
             return [Asset.from_dict(a) for a in response.json()['assets']]
@@ -15,6 +23,14 @@ class AssetService(AsyncBaseService):
                                             text=response.text)
 
     async def get_clock(self) -> datetime:
+        """Получает текущее время на сервере (локализованное).
+
+        Returns:
+            datetime: время на сервере.
+
+        Raises:
+            FinamResponseFailureException: если произошла ошибка запроса к серверу.
+        """
         response = await self._session.get('assets/clock')
         if response.status_code == 200:
             return formatted_datetime(response.json()['timestamp'])
@@ -22,6 +38,14 @@ class AssetService(AsyncBaseService):
                                             text=response.text)
 
     async def get_exchanges(self) -> list[Exchange]:
+        """Получает список доступных бирж, названия и mic коды.
+
+        Returns:
+            list[Exchange]: список доступных бирж.
+
+        Raises:
+            FinamResponseFailureException: если произошла ошибка запроса к серверу.
+        """
         response = await self._session.get('exchanges')
         if response.status_code == 200:
             return [Exchange.from_dict(e) for e in response.json()['exchanges']]
@@ -29,6 +53,17 @@ class AssetService(AsyncBaseService):
                                             text=response.text)
 
     async def get_asset(self, symbol: str) -> FullAsset:
+        """Получает подробную информацию по конкретному инструменту.
+
+        Args:
+            symbol (str): символ инструмента в формате ticker@mic.
+
+        Returns:
+            FullAsset: подробная информация по инструменту.
+
+        Raises:
+            FinamResponseFailureException: если произошла ошибка запроса к серверу.
+        """
         response = await self._session.get(f'assets/{symbol}',
                                            params={'symbol': symbol, 'account_id': self._account_id})
         if response.status_code == 200:
@@ -37,6 +72,18 @@ class AssetService(AsyncBaseService):
                                             text=response.text)
 
     async def get_asset_params(self, symbol: str, account_id: str = None):
+        """Получает торговые параметры по инструменту.
+
+        Args:
+            symbol (str): символ инструмента в формате ticker@mic.
+            account_id (Optional[str]): ID аккаунта, по умолчанию будут использован ID текущего аккаунта.
+
+        Returns:
+            AssetParams: торговые параметры по инструменту.
+
+        Raises:
+            FinamResponseFailureException: если произошла ошибка запроса к серверу.
+        """
         params = {'symbol': symbol, 'account_id': account_id if account_id else self._account_id}
         response = await self._session.get(f'assets/{symbol}/params', params=params)
         if response.status_code == 200:
@@ -48,6 +95,19 @@ class AssetService(AsyncBaseService):
                                 underlying_symbol: str,
                                 root: str = None,
                                 expiration_date: datetime = None) -> list[Option]:
+        """Получает цепочку опционов для базового актива.
+
+        Args:
+            underlying_symbol (str): символ базового инструмента в формате ticker@mic.
+            root (str):
+            expiration_date (datetime): дата экспирации.
+
+        Returns:
+            list[Option]: цепочка опционов.
+
+        Raises:
+            FinamResponseFailureException: если произошла ошибка запроса к серверу.
+        """
         params = {'underlying_symbol': underlying_symbol}
         if root:
             params['root'] = root
@@ -62,6 +122,18 @@ class AssetService(AsyncBaseService):
                                             text=response.text)
 
     async def get_schedule(self, symbol: str, only_today=False) -> list[ScheduleSession]:
+        """Получает расписание торгов для инструмента.
+
+        Args:
+            symbol (str): символ инструмента в формате ticker@mic.
+            only_today (Optional[bool]): отбор только сегодняшних сессий.
+
+        Returns:
+            list[ScheduleSession]: список торговых сессий.
+
+        Raises:
+            FinamResponseFailureException: если произошла ошибка запроса к серверу.
+        """
         response = await self._session.get(f'assets/{symbol}/schedule', params={'symbol': symbol})
         if response.status_code == 200:
             sessions = [ScheduleSession.from_dict(s) for s in response.json()['sessions']]
